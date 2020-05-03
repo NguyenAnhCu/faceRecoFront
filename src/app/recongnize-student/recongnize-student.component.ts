bc0a9d928e20f196a7a7f3303a47e2987228b96f
NGUYENANH CU <cnanh1996@gmail.com>
2020-05-03 14:55:58 +0200

    add chose photo and take photo with webcam in recognize-student

--- src/app/recongnize-student/recongnize-student.component.ts
+++ src/app/recongnize-student/recongnize-student.component.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, ElementRef, OnInit, Renderer2, ViewChild} from '@angular/core';
+import {HttpClientModule} from '@angular/common/http';
+import * as stream from 'stream';
 
 @Component({
   selector: 'app-recongnize-student',
@@ -7,9 +9,50 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RecongnizeStudentComponent implements OnInit {
 
-  constructor() { }
+  public captures: Array<any>;
+  constructor(private renderer: Renderer2) {
+    this.captures = [];
+  }
+  public selectedImage = null;
+  videoWidth = 0;
+  videoHeight = 0;
+  constraints = {
+    video: {
+      facingMode: 'environment',
+      width: { ideal: 4096 },
+      height: { ideal: 2160 }
+    }
+  };
+
+  @ViewChild('video', { static: true }) videoElement: ElementRef;
+  @ViewChild('canvas', { static: true }) canvas: ElementRef;
+
+  attachVideo(stream) {
+    this.renderer.setProperty(this.videoElement.nativeElement, 'srcObject', stream);
+  }
 
   ngOnInit() {
   }
 
+  startCamera() {
+    if (!!(navigator.mediaDevices && navigator.mediaDevices.getUserMedia)) {
+      navigator.mediaDevices.getUserMedia(this.constraints).then(this.attachVideo.bind(this)).catch(this.handleError);
+    } else {
+      alert('Sorry, camera not available.');
+    }
+  }
+
+  onFileSelected(event) {
+    this.selectedImage = event.target.files[0];
+    console.log(event);
+  }
+
+  capture() {
+    this.renderer.setProperty(this.canvas.nativeElement, 'width', this.videoWidth);
+    this.renderer.setProperty(this.canvas.nativeElement, 'height', this.videoHeight);
+    this.canvas.nativeElement.getContext('2d').drawImage(this.videoElement.nativeElement, 0, 0);
+  }
+  handleError(error) {
+    console.log('Error: ', error);
+  }
 }
